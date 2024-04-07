beb622fa1abc044ec570416d64b01c0b374d4c17
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-07 14:24:02 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/Cloud-services-college-project",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
