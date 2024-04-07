a18a1121c36008735696d66f977fb7ece888902b
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-07 15:21:23 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = () => {
+export const nextConfig = () => {
     return {
     output: "export",
 }
 };
-export default nextConfig;
+
 
 
 
