bfb5cf86973512d5781e4ab3ff43ae750d63df1f
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-07 13:00:08 +0600

    0704 content update

--- app/components/cardslist/cardslist.jsx
+++ app/components/cardslist/cardslist.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 import Card from '../card/card'
+import { data } from '@/app/data/data'
 import Styles from './cardslist.module.css'
 export default function Cardslist() {
   return (
     <section className={Styles["ListWrapper"]}>
     <section className={Styles["CardsList"]}>
-    <Card imageLink ="" name="Microsoft Onedrive"/>
-    <Card imageLink ="" name="Google Workspace"/>
-    <Card imageLink ="" name="Youtube"/>
-    <Card imageLink ="" name="Figma"/>
-    <Card imageLink ="" name="Github"/>
-    <Card imageLink ="" name="Zoom"/>
+      
+    <Card imageLink ="./images/servicesimages/ms-onedrive.svg" name="Microsoft Onedrive"/>
+    
+    <Card imageLink ="./images/servicesimages/googleworkspace.svg" name="Google Workspace"/>
+    <Link href={`../../servicespages/Youtube/page.js`} className="Linklinks">
+    <Card imageLink ="./images/servicesimages/youtube.svg" data={data} name="Youtube"/>
+    </Link>
+    <Card imageLink ="./images/servicesimages/figma.svg" name="Figma"/>
+    <Card imageLink ="./images/servicesimages/github.svg" name="Github"/>
+    <Card imageLink ="./images/servicesimages/zoom.svg" name="Zoom"/>
+    <Card imageLink ="./images/servicesimages/gmail.svg" name="Gmail"/>
 </section>
 </section>
   )
