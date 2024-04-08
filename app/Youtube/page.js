9267c0fc45bc7f645db0ce6b7528e22f306e4a97
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-08 17:52:18 +0600

    Pages for all services made in root folder

--- app/Youtube/page.js
+++ app/Youtube/page.js
@@ -1,4 +1,4 @@
-export default function ServicePage() {
+export default function YoutubePage() {
     return (
         <h1>3121232321</h1>
     )
--- app/components/cardslist/cardslist.jsx
+++ app/components/cardslist/cardslist.jsx
@@ -7,17 +7,33 @@ export default function Cardslist() {
   return (
     <section className={Styles["ListWrapper"]}>
     <section className={Styles["CardsList"]}>
-      
-    <Card imageLink ="./images/ms-onedrive.svg" name="Microsoft Onedrive"/>
-    
-    <Card imageLink ="./images/googleworkspace.svg" name="Google Workspace"/>
-    <Link href={`/Youtube`} className="Linklinks">
-    <Card imageLink ="./images/youtube.svg" data={data} name="Youtube"/>
-    </Link>
-    <Card imageLink ="./images/figma.svg" name="Figma"/>
-    <Card imageLink ="./images/github.svg" name="Github"/>
-    <Card imageLink ="./images/zoom.svg" name="Zoom"/>
-    <Card imageLink ="./images/gmail.svg" name="Gmail"/>
+
+                      <Link href="/Onedrive" className="LInklinks">
+                    <Card imageLink ="./images/ms-onedrive.svg" name="Microsoft Onedrive"/>
+                    </Link>
+
+                                  <Link href="/Workspace" className='Linklinks'>
+                                  <Card imageLink ="./images/googleworkspace.svg" name="Google Workspace"/>
+                                  </Link>
+
+                        <Link href={`/Youtube`} className="Linklinks">
+                        <Card imageLink ="./images/youtube.svg" data={data} name="Youtube"/>
+                        </Link>
+
+                                            <Link href="/Figma" className='Linklinks'>                   
+                                            <Card imageLink ="./images/figma.svg" name="Figma"/>
+                                            </Link>
+
+                <Link href="/Github" className='Linklinks'>                                       
+                <Card imageLink ="./images/github.svg" name="Github"/>
+                </Link> 
+                
+                                                <Link href="/Zoom" className="Linklinks">
+                                                <Card imageLink ="./images/zoom.svg" name="Zoom"/>
+                                                </Link>
+                      <Link href="/Gmail" className='Linklinks'>                                           
+                      <Card imageLink ="./images/gmail.svg" name="Gmail"/>
+                      </Link> 
 </section>
 </section>
   )
